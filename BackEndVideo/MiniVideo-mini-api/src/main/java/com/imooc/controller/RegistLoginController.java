e3ac5ce2511e37363dae8711b448ceb023f24363
LingweiLuo <564258080@qq.com>
2020-03-31 00:04:40 -0400

    update

--- BackEndVideo/MiniVideo-mini-api/src/main/java/com/imooc/controller/RegistLoginController.java
+++ BackEndVideo/MiniVideo-mini-api/src/main/java/com/imooc/controller/RegistLoginController.java
@@ -49,8 +49,8 @@ public class RegistLoginController extends BasicController {
 		} else {
 			return IMoocJSONResult.errorMsg("用户名已经存在，请换一个再试");
 		}
-		
-//		user.setPassword("");
+
+		user.setPassword("");
 		
 //		String uniqueToken = UUID.randomUUID().toString();
 //		redis.set(USER_REDIS_SESSION + ":" + user.getId(), uniqueToken, 1000 * 60 * 30);
@@ -60,8 +60,9 @@ public class RegistLoginController extends BasicController {
 //		userVO.setUserToken(uniqueToken);
 		
 //		UsersVO userVO = setUserRedisSessionToken(user);
-		
-		return IMoocJSONResult.ok();
+
+
+		return IMoocJSONResult.ok(user);
 	}
 	
 //	public UsersVO setUserRedisSessionToken(Users userModel) {
@@ -73,34 +74,34 @@ public class RegistLoginController extends BasicController {
 //		userVO.setUserToken(uniqueToken);
 //		return userVO;
 //	}
-//
-//	@ApiOperation(value="用户登录", notes="用户登录的接口")
-//	@PostMapping("/login")
-//	public IMoocJSONResult login(@RequestBody Users user) throws Exception {
-//		String username = user.getUsername();
-//		String password = user.getPassword();
-//
-////		Thread.sleep(3000);
-//
-//		// 1. 判断用户名和密码必须不为空
-//		if (StringUtils.isBlank(username) || StringUtils.isBlank(password)) {
-//			return IMoocJSONResult.ok("用户名或密码不能为空...");
-//		}
-//
-//		// 2. 判断用户是否存在
-//		Users userResult = userService.queryUserForLogin(username,
-//				MD5Utils.getMD5Str(user.getPassword()));
-//
-//		// 3. 返回
-//		if (userResult != null) {
-//			userResult.setPassword("");
+
+	@ApiOperation(value="用户登录", notes="用户登录的接口")
+	@PostMapping("/login")
+	public IMoocJSONResult login(@RequestBody Users user) throws Exception {
+		String username = user.getUsername();
+		String password = user.getPassword();
+
+//		Thread.sleep(3000);
+
+		// 1. 判断用户名和密码必须不为空
+		if (StringUtils.isBlank(username) || StringUtils.isBlank(password)) {
+			return IMoocJSONResult.ok("用户名或密码不能为空...");
+		}
+
+		// 2. 判断用户是否存在
+		Users userResult = userService.queryUserForLogin(username,
+				MD5Utils.getMD5Str(user.getPassword()));
+
+		// 3. 返回
+		if (userResult != null) {
+			userResult.setPassword("");
 //			UsersVO userVO = setUserRedisSessionToken(userResult);
-//			return IMoocJSONResult.ok(userVO);
-//		} else {
-//			return IMoocJSONResult.errorMsg("用户名或密码不正确, 请重试...");
-//		}
-//	}
-//
+			return IMoocJSONResult.ok(userResult);
+		} else {
+			return IMoocJSONResult.errorMsg("用户名或密码不正确, 请重试...");
+		}
+	}
+
 //	@ApiOperation(value="用户注销", notes="用户注销的接口")
 //	@ApiImplicitParam(name="userId", value="用户id", required=true,
 //						dataType="String", paramType="query")
